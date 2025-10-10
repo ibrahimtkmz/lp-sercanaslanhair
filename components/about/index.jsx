4bab636319742d0805699b76694a6ada096fe8db
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-10-10 10:16:28 +0300

    Update index.jsx

--- components/about/index.jsx
+++ components/about/index.jsx
@@ -24,7 +24,9 @@ export default function About({ about, phone, wp_message }) {
               />
             </Link>
             <Image src={"/images/d.png"} width={150} height={137.5} style={{ height: "auto" }} alt="doctor"
-            quality={60} <p>Bu bir örnek yazıdır.Bu bir örnek yazıdır.Bu bir örnek yazıdır.</p>
+            quality={60} 
+
+              <p>Treatment takes place at partner’s clinics which have the hauthorization for a medical tourism.</p>
             />
           </article>
        
