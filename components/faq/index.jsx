a5d5f54ef51b211b03ea0aa1df30a5704c1b1145
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-03 13:05:38 +0300

    Update index.jsx

--- components/faq/index.jsx
+++ components/faq/index.jsx
@@ -1,36 +1,47 @@
 import Image from 'next/image'
 import './styles.css'
 import { useState } from 'react'
+import { usePathname } from 'next/navigation'
 import WpLink from '../wp'
 
 export default function Faq({ faq, phone, wp_message }) {
      const [active, setActive] = useState(0)
+     const pathname = usePathname()
+
+     // Sayfa yoluna göre resim belirle
+     const imageSrc =
+          pathname === '/dental-treatment-in-turkey'
+               ? '/images/dental-care2.jpeg'
+               : '/images/f.webp'
+
      return (
-          <section
-               className="faq"
-               id="faq"
-          >
+          <section className="faq" id="faq">
                <div className="faq-inner">
                     <div className="faq-content">
                          <h2 dangerouslySetInnerHTML={{ __html: faq.title }} />
                          <ul>
-                              {faq.all_questions.map((item, i) => {
-                                   return (
-                                        <li
-                                             key={i}
-                                             className={active === i ? 'active' : ''}
+                              {faq.all_questions.map((item, i) => (
+                                   <li key={i} className={active === i ? 'active' : ''}>
+                                        <button
+                                             className="faq-question"
+                                             onClick={() => setActive(i)}
                                         >
-                                             <button
-                                                  className="faq-question"
-                                                  onClick={() => setActive(i)}
-                                             >
-                                                  <div className="faq-plus">{active === i ? '+' : '-'}</div>
-                                                  <h3 dangerouslySetInnerHTML={{ __html: item.question }} />
-                                             </button>
-                                             <p dangerouslySetInnerHTML={{ __html: item.answer }} />
-                                        </li>
-                                   )
-                              })}
+                                             <div className="faq-plus">
+                                                  {active === i ? '+' : '-'}
+                                             </div>
+                                             <h3
+                                                  dangerouslySetInnerHTML={{
+                                                       __html: item.question,
+                                                  }}
+                                             />
+                                        </button>
+                                        <p
+                                             dangerouslySetInnerHTML={{
+                                                  __html: item.answer,
+                                             }}
+                                        />
+                                   </li>
+                              ))}
                          </ul>
                          <WpLink
                               title={faq.button}
@@ -39,9 +50,12 @@ export default function Faq({ faq, phone, wp_message }) {
                          />
                     </div>
                     <div className="faq-img">
-                         <img
-                              src={'/images/f.webp'}
-                              alt="Hair transplant in Turkey"
+                         <Image
+                              src={imageSrc}
+                              alt="FAQ section image"
+                              width={500}
+                              height={500}
+                              quality={70}
                          />
                     </div>
                </div>
