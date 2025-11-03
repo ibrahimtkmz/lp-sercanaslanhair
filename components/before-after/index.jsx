aaa14d1d8d0273f95c69be820b85c02f032d979f
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-03 16:19:24 +0300

    Update index.jsx

--- components/before-after/index.jsx
+++ components/before-after/index.jsx
@@ -1,17 +1,36 @@
+"use client";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import WpLink from "../wp";
 import "./styles.css";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function BeforeAfter({
-  before_after,
-  wp_message,
-  phone,
-  lang
-}) {
+export default function BeforeAfter({ before_after, wp_message, phone, lang }) {
+  const pathname = usePathname();
+  const [images, setImages] = useState(before_after.images);
+
+  useEffect(() => {
+    if (pathname?.toLowerCase().includes("dental-treatment-in-turkey")) {
+      setImages([
+        "/images/d1.jpeg",
+        "/images/d2.jpeg",
+        "/images/d3.jpeg",
+        "/images/d4.jpeg",
+        "/images/d5.jpeg",
+        "/images/d6.jpeg",
+        "/images/d7.jpeg",
+        "/images/d8.jpeg",
+      ]);
+    } else {
+      setImages(before_after.images); // diğer sayfalar
+    }
+  }, [pathname, before_after.images]);
+
   const link = `https://wa.me/${phone}?text=${wp_message}`;
+  
   var settings = {
     dots: true,
     lazyLoad: true,
@@ -31,6 +50,7 @@ export default function BeforeAfter({
       },
     ],
   };
+
   return (
     <section className="before-after" id="beforeAfter">
       <div className="before-after-inner">
@@ -45,24 +65,19 @@ export default function BeforeAfter({
         </article>
         <div className="ba-slider">
           <Slider {...settings}>
-            {before_after.images.map((image, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`before-after-card show`}
-                >
-                  <Image
-                    src={image}
-                    width={400}
-                    height={300}
-                    quality={60}
-                    alt="before-after"
-                  />
-                </div>
-              );
-            })}
+            {images.map((image, index) => (
+              <div key={index} className={`before-after-card show`}>
+                <Image
+                  src={image}
+                  width={400}
+                  height={300}
+                  quality={60}
+                  alt="before-after"
+                />
+              </div>
+            ))}
           </Slider>
-          <p className="ba-privacy"> {before_after.privacy}</p>
+          <p className="ba-privacy">{before_after.privacy}</p>
         </div>
       </div>
     </section>
