9a3fb39c7d57671a6b0e28a76958dcd9f83cdeb8
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-21 13:34:09 +0300

    Update index.jsx

--- components/wp/index.jsx
+++ components/wp/index.jsx
@@ -2,22 +2,20 @@
 import './styles.css'
 import Image from 'next/image'
 
-// "phone" yanına eşittir koyarak varsayılan numarayı yazıyoruz.
-// Böylece yukarıdan veri gelmese bile (undefined olsa bile) bu numara devreye girer.
-export default function WpLink({ title, wp_message, phone = "905467372284" }) {
-     return (
-          <a
-               className="wp-link"
-               href={`https://wa.me/${phone}?text=${wp_message}`}
-          >
-               <Image
-                    src={'/images/button-arrow.png'}
-                    width={52}
-                    height={52}
-                    alt="wp button arrow"
-                    unoptimized
-               />
-               <span>{title}</span>
-          </a>
-     )
+export default function WpLink({ title, wp_message, phone }) {
+  return (
+    <a
+      className="wp-link"
+      href={`https://wa.me/${phone}?text=${wp_message}`}
+    >
+      <Image
+        src={'/images/button-arrow.png'}
+        width={52}
+        height={52}
+        alt="wp button arrow"
+        unoptimized
+      />
+      <span>{title}</span>
+    </a>
+  )
 }
