c92d523966b3bab9a61caf51b21a8e84e4b65764
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-21 13:38:08 +0300

    Update index.jsx

--- components/wp/index.jsx
+++ components/wp/index.jsx
@@ -3,19 +3,19 @@ import './styles.css'
 import Image from 'next/image'
 
 export default function WpLink({ title, wp_message, phone }) {
-  return (
-    <a
-      className="wp-link"
-      href={`https://wa.me/${phone}?text=${wp_message}`}
-    >
-      <Image
-        src={'/images/button-arrow.png'}
-        width={52}
-        height={52}
-        alt="wp button arrow"
-        unoptimized
-      />
-      <span>{title}</span>
-    </a>
-  )
+     return (
+          <a
+               className="wp-link"
+               href={`https://wa.me/${phone}?text=${wp_message}`}
+          >
+               <Image
+                    src={'/images/button-arrow.png'}
+                    width={52}
+                    height={52}
+                    alt="wp button arrow"
+                    unoptimized
+               />
+               <span>{title}</span>
+          </a>
+     )
 }
