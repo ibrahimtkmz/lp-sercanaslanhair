c691e716e6ce59397197ffbb073373a0bfe159a1
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-21 10:32:50 +0300

    Update page.jsx

--- app/(inside)/hair-transplant-in-turkey-esp/page.jsx
+++ app/(inside)/hair-transplant-in-turkey-esp/page.jsx
@@ -1,34 +1,34 @@
-import data from '../../../data/ru.json'
+import data from '../../../data/es.json'          // en.json -> es.json
 import phone from '../../../data/phone.json'
-import lead from '../../../data/lead/en-lead.json'
+import lead from '../../../data/lead/es-lead.json' // en-lead.json -> es-lead.json
 import HomeClient from '../../../components/HomeClient'
 
 export default function Page() {
-     return (
-          <>
-               <HomeClient
-                    lang={data.lang}
-                    phone={phone.en}
-                    wp_message={data.wp.wp_message}
-                    form={data.form}
-                    lead={lead}
-                    visible_en={phone.visible_en}
-                    variant={data.variant}
-                    header={data.header}
-                    about={data.about}
-                    hero={data.hero}
-                    steps={data.steps}
-                    reviews={data.reviews}
-                    price={data.price}
-                    services={data.services}
-                    before_after={data.before_after}
-                    certificates={data.certificates}
-                    icons={data.iconsGroup}
-                    hospital={data.hospital}
-                    types={data.procedures}
-                    faq={data.faq}
-                    footer={data.footer}
-               />
-          </>
-     )
+      return (
+           <>
+                <HomeClient
+                     lang={data.lang}
+                     phone={phone.es}               // phone.en -> phone.es
+                     wp_message={data.wp.wp_message}
+                     form={data.form}
+                     lead={lead}
+                     visible_es={phone.visible_es}  // visible_en -> visible_es (Aşağıdaki nota dikkat edin)
+                     variant={data.variant}
+                     header={data.header}
+                     about={data.about}
+                     hero={data.hero}
+                     steps={data.steps}
+                     reviews={data.reviews}
+                     price={data.price}
+                     services={data.services}
+                     before_after={data.before_after}
+                     certificates={data.certificates}
+                     icons={data.iconsGroup}
+                     hospital={data.hospital}
+                     types={data.procedures}
+                     faq={data.faq}
+                     footer={data.footer}
+                />
+           </>
+      )
 }
