ef2ffba392b3d2afb1873d479ccb50ffedcd826a
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-21 13:15:55 +0300

    Update index.jsx

--- components/wp-sticky/index.jsx
+++ components/wp-sticky/index.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-// Eğer CSS dosyanız varsa importu burada kalsın, yoksa silebilirsiniz:
 
 export default function WpSticky({ wp_message, phone }) {
   
