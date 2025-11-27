882191013ea5c932036bd6c4474ba60e965f8317
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-11-27 13:52:33 +0300

    Update page.jsx

--- app/(inside)/hair-transplant-in-turkey/page.jsx
+++ app/(inside)/hair-transplant-in-turkey/page.jsx
@@ -5,31 +5,33 @@ import HomeClient from '../../../components/HomeClient'
 import NewYearButton from '../../../components/NewYearButton'
 
 export default function Page() {
-     return (
-          <>
-               <HomeClient
-                    lang={data.lang}
-                    phone={phone.en}
-                    wp_message={data.wp.wp_message}
-                    form={data.form}
-                    lead={lead}
-                    visible_en={phone.visible_en}
-                    variant={data.variant}
-                    header={data.header}
-                    about={data.about}
-                    hero={data.hero}
-                    steps={data.steps}
-                    reviews={data.reviews}
-                    price={data.price}
-                    services={data.services}
-                    before_after={data.before_after}
-                    certificates={data.certificates}
-                    icons={data.iconsGroup}
-                    hospital={data.hospital}
-                    types={data.procedures}
-                    faq={data.faq}
-                    footer={data.footer}
-               />
-          </>
-     )
+  return (
+    <>
+      <HomeClient
+        lang={data.lang}
+        phone={phone.en}
+        wp_message={data.wp.wp_message}
+        form={data.form}
+        lead={lead}
+        visible_en={phone.visible_en}
+        variant={data.variant}
+        header={data.header}
+        about={data.about}
+        hero={data.hero}
+        steps={data.steps}
+        reviews={data.reviews}
+        price={data.price}
+        services={data.services}
+        before_after={data.before_after}
+        certificates={data.certificates}
+        icons={data.iconsGroup}
+        hospital={data.hospital}
+        types={data.procedures}
+        faq={data.faq}
+        footer={data.footer}
+      />
+
+      <NewYearButton />
+    </>
+  );
 }
